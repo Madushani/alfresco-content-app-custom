da33174414585be373d71cdf89e4a7b35ebb5a44
Asirika <srkcosta@gmail.com>
2020-05-15 10:32:59 +1000

    Email As Link

--- src/app/dialogs/my-extension-dialog/my-extension-dialog.component.ts
+++ src/app/dialogs/my-extension-dialog/my-extension-dialog.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
 
+import { Store } from '@ngrx/store';
+import { AppStore, getAppSelection } from '@alfresco/aca-shared/store';
+import { take } from 'rxjs/operators';
+import { SharedLinkEntry } from '@alfresco/js-api';
+import { SharedLinksApiService, AppConfigService } from '@alfresco/adf-core';
+
 
 @Component({
   selector: 'aca-my-extension-dialog',
@@ -11,10 +17,18 @@ import { MatDialogRef } from '@angular/material/dialog';
 
 export class MyExtensionDialogComponent implements OnInit {
 
+  nodeId: string;
+  nodeName: string;
+  hostName: string;
 
+  sharednode: string;
+  private sharedLinksApiService;
+  private appConfigService;
   constructor(
-    public dialogRef: MatDialogRef<MyExtensionDialogComponent>) {
-
+    public dialogRef: MatDialogRef<MyExtensionDialogComponent>, private store: Store<AppStore>
+    , sharedLinksApiService: SharedLinksApiService, appConfigService: AppConfigService) {
+    this.sharedLinksApiService = sharedLinksApiService;
+    this.appConfigService = appConfigService;
   }
 
   ngOnInit() {
@@ -23,8 +37,36 @@ export class MyExtensionDialogComponent implements OnInit {
   }
   openDialog() {
 
-    // window.location = 'mailto:?subject=' + encodeURIComponent("Billing statement Alfresco.xlsx") + '&body='
-    //   + encodeURIComponent("http://localhost:4200/#/libraries/1f36ade1-9f2b-4f95-a0be-5ca28d98ffa5/(viewer:view/ba2897d4-54e3-44a2-b1d3-d23ab9c07ee3)?location=%2Flibraries%2F1f36ade1-9f2b-4f95-a0be-5ca28d98ffa5");
+    this.store
+      .select(getAppSelection)
+      .pipe(take(1))
+      .subscribe(selection => {
+        this.nodeId =
+          (selection.file as SharedLinkEntry).entry.id;
+        this.nodeName = (selection.file as SharedLinkEntry).entry.name;
+
+      });
+
+    this.hostName = this.appConfigService.config.ecmHost;
+
+    this.sharedLinksApiService.getSharedLinks().subscribe(responce => {
+
+      let entries = responce.list.entries;
+      const length = responce.list.entries.length;
+      for (let i = 0; i < length; i++) {
+
+        if (entries[i].entry.nodeId == this.nodeId) {
+          let link = this.hostName + "/#/preview/s/" + entries[i].entry.id;
+          window.location = 'mailto:?subject=' + encodeURIComponent(this.nodeName) + '&body=' + encodeURIComponent(link);
+        }
+      }
+    });
+    this.sharedLinksApiService.createSharedLinks(this.nodeId).pipe(take(1)).subscribe(sharedlink => {
+      this.sharednode = sharedlink.entry.id;
+      let link = this.hostName + "/#/preview/s/" + this.sharednode;
+      window.location = 'mailto:?subject=' + encodeURIComponent(this.nodeName) + '&body=' + encodeURIComponent(link);
+    });
+
     this.dialogRef.close();
 
   }
